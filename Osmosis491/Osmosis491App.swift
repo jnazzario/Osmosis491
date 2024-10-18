a3be21adaafda87c9da905b470a7938670a2bb44
jnazzario <jnazzario0@saddleback.edu>
2024-10-17 17:01:46 -0700

    Initial Commit

---
+++ Osmosis491/Osmosis491App.swift
@@ -0,0 +1,17 @@
+//
+//  Osmosis491App.swift
+//  Osmosis491
+//
+//  Created by Juli Nazzario on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Osmosis491App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
