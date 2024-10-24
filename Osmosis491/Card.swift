91176c3ad8b5d56099cfe859177cdb6fb8bdc33e
Juli Nazzario <98191405+jnazzario@users.noreply.github.com>
2024-10-23 17:43:00 -0700

    Update Card.swift

--- Osmosis491/Card.swift
+++ Osmosis491/Card.swift
@@ -1,6 +1,4 @@
 import Foundation
-
-// MARK: - Card Model
 struct Card: Identifiable {
     let id = UUID()
     let suit: Suit
@@ -14,7 +12,7 @@ struct Card: Identifiable {
         case ace = "A", two = "2", three = "3", four = "4", five = "5", six = "6", seven = "7", eight = "8", nine = "9", ten = "10", jack = "J", queen = "Q", king = "K"
     }
 
-    // Computed property to generate the image name for each card
+    // Generates the image name for each card
     var imageName: String {
         return "\(rank.rawValue)_of_\(suit.rawValue)"
     }
